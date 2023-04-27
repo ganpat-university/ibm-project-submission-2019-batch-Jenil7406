9c6c5baee3b554e571dbfb49095770caede20244
Jenil7406 <57008800+Jenil7406@users.noreply.github.com>
2023-04-27 22:22:50 +0530

    Delete events directory

--- events/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Events
-
-class EventsAdmin(admin.ModelAdmin):
-    list_display = ('event_name','event_description','max_tickets','start_date','end_date','event_created','account_id')
-
-# Register your models here.
-admin.site.register(Events,EventsAdmin)
-
